503ff14db3b26506d7837971b658e8f72af4bf56
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-15 10:33:18 -0500

    initial commit

---
+++ ICE2/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week2/MainActivity.kt
@@ -0,0 +1,13 @@
+package ca.lakeheadu.comp3025g_w2024_week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
