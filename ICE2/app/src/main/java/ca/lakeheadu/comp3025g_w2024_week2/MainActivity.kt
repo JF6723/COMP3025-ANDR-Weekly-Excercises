bbd86011ca36888458d8e848155a173abdafd6f2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-15 11:29:23 -0500

    added UI References

--- ICE2/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week2/MainActivity.kt
+++ ICE2/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week2/MainActivity.kt
@@ -2,6 +2,9 @@ package ca.lakeheadu.comp3025g_w2024_week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +12,8 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeTextView = findViewById<TextView>(R.id.clickMeTextView)
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
     }
 }
