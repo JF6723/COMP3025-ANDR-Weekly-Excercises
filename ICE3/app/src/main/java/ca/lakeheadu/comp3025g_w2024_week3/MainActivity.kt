a55e34c04932df61f78a2540fc445bed71ac080f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-22 09:18:21 -0500

    added project files for ICE3

---
+++ ICE3/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week3/MainActivity.kt
@@ -0,0 +1,19 @@
+/*
+ NAME: Tom Tsiliopoulos
+ DATE: January 22, 2024
+ ID  : 1234567
+ */
+
+package ca.lakeheadu.comp3025g_w2024_week3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
