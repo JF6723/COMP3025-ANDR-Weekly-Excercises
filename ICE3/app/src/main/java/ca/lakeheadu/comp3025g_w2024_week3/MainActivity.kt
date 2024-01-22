c7f36e2b8f96052313721a7382dd3f3fa0237755
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-01-22 10:40:29 -0500

    added shared event handler for operator buttons

--- ICE3/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week3/MainActivity.kt
+++ ICE3/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week3/MainActivity.kt
@@ -8,6 +8,9 @@ package ca.lakeheadu.comp3025g_w2024_week3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity()
 {
@@ -15,5 +18,17 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // create references to button Views
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        val percentButton = findViewById<Button>(R.id.percentButton)
+
+        clearButton.setOnClickListener { view -> processOperatorButtons(view)}
+        percentButton.setOnClickListener { view ->processOperatorButtons(view)}
+    }
+
+    private fun processOperatorButtons(view: View)
+    {
+        Log.i("operator", view.tag.toString())
     }
 }
