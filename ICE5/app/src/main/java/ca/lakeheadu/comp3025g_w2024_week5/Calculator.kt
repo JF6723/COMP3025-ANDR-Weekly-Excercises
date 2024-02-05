2a891113a39c6de1e4b804b9b6653e9eec6388de
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2024-02-05 11:00:17 -0500

    added Calculator Class

---
+++ ICE5/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week5/Calculator.kt
@@ -0,0 +1,72 @@
+package ca.lakeheadu.comp3025g_w2024_week5
+
+import android.view.View
+import ca.lakeheadu.comp3025g_w2024_week5.databinding.ActivityMainBinding
+
+class Calculator (binding: ActivityMainBinding)
+{
+    private var m_binding: ActivityMainBinding
+    private var m_resultString: String
+
+    init {
+        this.m_binding = binding
+        this.m_resultString = ""
+
+        initializeOnClickListeners()
+    }
+
+    private fun initializeOnClickListeners() {
+        // Listeners for Extras Buttons
+        m_binding.clearButton.setOnClickListener { view -> processExtraButtons(view) }
+        m_binding.percentButton.setOnClickListener { view -> processExtraButtons(view) }
+        m_binding.backspaceButton.setOnClickListener { view -> processExtraButtons(view) }
+        m_binding.plusminusButton.setOnClickListener { view -> processExtraButtons(view) }
+
+        // Listeners for the Operator Buttons
+        m_binding.divideButton.setOnClickListener { view -> processOperatorButtons(view) }
+        m_binding.multiplyButton.setOnClickListener { view -> processOperatorButtons(view) }
+        m_binding.addButton.setOnClickListener { view -> processOperatorButtons(view) }
+        m_binding.subtractButton.setOnClickListener { view -> processOperatorButtons(view) }
+        m_binding.equalsButton.setOnClickListener { view -> processOperatorButtons(view) }
+
+        // Listeners for the Number Buttons (and decimal)
+        m_binding.zeroButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.oneButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.twoButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.threeButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.fourButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.fiveButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.sixButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.sevenButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.eightButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.nineButton.setOnClickListener { view -> processNumberButtons(view) }
+        m_binding.decimalButton.setOnClickListener { view -> processNumberButtons(view) }
+    }
+
+    private fun processOperatorButtons(view: View)
+    {
+        this.m_binding.resultTextView.text = view.tag.toString()
+    }
+
+    private fun processExtraButtons(view: View)
+    {
+        this.m_binding.resultTextView.text = view.tag.toString()
+    }
+
+    private fun processNumberButtons(view: View)
+    {
+        if(view.tag.toString() == ".")
+        {
+            if(!m_resultString.contains("."))
+            {
+                m_resultString += view.tag.toString()
+            }
+        }
+        else
+        {
+            m_resultString += view.tag.toString()
+        }
+
+        this.m_binding.resultTextView.text = m_resultString
+    }
+}
--- ICE5/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week5/MainActivity.kt
+++ ICE5/app/src/main/java/ca/lakeheadu/comp3025g_w2024_week5/MainActivity.kt
@@ -14,7 +14,8 @@ import ca.lakeheadu.comp3025g_w2024_week5.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityMainBinding
-    private var resultString: String = ""
+    //private var resultString: String = ""
+    private lateinit var calculator: Calculator
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -22,65 +23,7 @@ class MainActivity : AppCompatActivity()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initializeOnClickListeners()
-    }
+        calculator = Calculator(binding)
 
-    private fun initializeOnClickListeners() {
-        // Listeners for Extras Buttons
-        binding.clearButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.percentButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.backspaceButton.setOnClickListener { view -> processExtraButtons(view) }
-        binding.plusminusButton.setOnClickListener { view -> processExtraButtons(view) }
-
-        // Listeners for the Operator Buttons
-        binding.divideButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.multiplyButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.addButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.subtractButton.setOnClickListener { view -> processOperatorButtons(view) }
-        binding.equalsButton.setOnClickListener { view -> processOperatorButtons(view) }
-
-        // Listeners for the Number Buttons (and decimal)
-        binding.zeroButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.oneButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.twoButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.threeButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.fourButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.fiveButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.sixButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.sevenButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.eightButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.nineButton.setOnClickListener { view -> processNumberButtons(view) }
-        binding.decimalButton.setOnClickListener { view -> processNumberButtons(view) }
-    }
-
-    /**
-     * This method processes any operator button that is clicked
-     * @param view [View]
-     */
-    private fun processOperatorButtons(view: View)
-    {
-        binding.resultTextView.text = view.tag.toString()
-    }
-
-    private fun processExtraButtons(view: View)
-    {
-        binding.resultTextView.text = view.tag.toString()
-    }
-
-    private fun processNumberButtons(view: View)
-    {
-        if(view.tag.toString() == ".")
-        {
-            if(!resultString.contains("."))
-            {
-                resultString += view.tag.toString()
-            }
-        }
-        else
-        {
-            resultString += view.tag.toString()
-        }
-
-        binding.resultTextView.text = resultString
     }
 }
